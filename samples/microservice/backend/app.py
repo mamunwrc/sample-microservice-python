284790f39e204540aead057ef4593ae0190fe5b7
S M Mamun Hossain <103784108+mamunwrc@users.noreply.github.com>
2023-03-27 00:48:04 +0600

    Revert "Prepared basic messaging system"

--- samples/microservice/backend/app.py
+++ samples/microservice/backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask,request,render_template,redirect
 import psycopg2
 import os
 import time
@@ -10,85 +10,62 @@ DB_HOST = os.environ["DB_HOST"]
 
 app = Flask(__name__)
 
-
 def init_tables():
 
-    CREATE_TABLE = (
-        "CREATE TABLE IF NOT EXISTS REVIEWS (name TEXT NOT NULL, review TEXT NOT NULL)"
-    )
-    conn = psycopg2.connect(
-        host=DB_HOST, database=DB, user=DB_USER, password=DB_PASSWORD
-    )
+    CREATE_TABLE = "CREATE TABLE IF NOT EXISTS REVIEWS (name TEXT NOT NULL, review TEXT NOT NULL)"
+    conn = psycopg2.connect(host=DB_HOST,database=DB, user=DB_USER, password=DB_PASSWORD)
     cursor = conn.cursor()
     cursor.execute(CREATE_TABLE)
     conn.commit()
     cursor.close()
     conn.close()
 
-
-def add_review(name, review):
+def add_review(name,review):
 
     ADD_REVIEW = "INSERT INTO REVIEWS (name,review) VALUES (%s,%s)"
-    conn = psycopg2.connect(
-        host=DB_HOST, database=DB, user=DB_USER, password=DB_PASSWORD
-    )
+    conn = psycopg2.connect(host=DB_HOST,database=DB, user=DB_USER, password=DB_PASSWORD)
     cursor = conn.cursor()
-    cursor.execute(ADD_REVIEW, (name, review))
+    cursor.execute(ADD_REVIEW,(name,review))
     conn.commit()
     cursor.close()
     conn.close()
-
 
 def load_reviews():
 
     reviews = []
 
     LOAD_REVIEW = "SELECT * from REVIEWS"
-    conn = psycopg2.connect(
-        host=DB_HOST, database=DB, user=DB_USER, password=DB_PASSWORD
-    )
+    conn = psycopg2.connect(host=DB_HOST,database=DB, user=DB_USER, password=DB_PASSWORD)
     cursor = conn.cursor()
     cursor.execute(LOAD_REVIEW)
     results = cursor.fetchall()
 
     for row in results:
-        reviews.append((row[0], row[1]))
+        reviews.append((row[0],row[1]))
     cursor.close()
     conn.close()
 
     return reviews
 
-
-@app.route("/reviews/add", methods=["POST"])
+@app.route("/reviews/add",methods=["POST"])
 def addreview():
 
+    
     name = request.form["name"]
     review = request.form["review"]
 
-    add_review(name, review)
+    add_review(name,review)
 
-    return {"success": True}
+    return {"success":True}
 
-
-# @app.route("/reviews/add", methods=["POST"])
-# def addreview(data):
-#     name = data["name"]
-#     review = data["review"]
-
-#     add_review(name, review)
-
-#     return {"success": True}
-
-
-@app.route("/reviews/list", methods=["POST"])
+@app.route("/reviews/list",methods=["POST"])
 def listreviews():
 
     reviews = load_reviews()
 
-    return {"success": True, "list": reviews}
+    return {"success":True,"list":reviews}
 
-
-# Deferred execution by 20 seconds to allow database to initialize
+#Deferred execution by 20 seconds to allow database to initialize
 time.sleep(20)
 init_tables()
-app.run(host="0.0.0.0", port=5000)
+app.run(host="0.0.0.0",port=5000)
